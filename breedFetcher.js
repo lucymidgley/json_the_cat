35f648cf301ecb4b52645d08cf6208a4ed11acc6
Lucy <lucymidgley13@gmail.com>
2020-01-16 21:07:03 +0000

    wrote some test code to check the function was performig correctly and chnaged the breed fetcher function so it outputs an error if the breed does not exist

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,22 @@
 const request = require('request');
-const args = process.argv;
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[2], (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed not found');
+
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        error = true;
+        callback('Breed does not exist', null);
+      } else {
+        callback(null, data[0].description);
+      }
     }
   }
-});
+  );
+};
+module.exports = { fetchBreedDescription };
+
